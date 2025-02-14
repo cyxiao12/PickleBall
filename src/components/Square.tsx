d68260ed728afa97eba0b4318763f095c8d2d55f
“cyxiao12” <cyxiao12@gmail.com>
2025-02-14 16:11:00 -0500

    click on the tiktac toe w x

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 export default function Square() {
+  const [value, setValue] = useState<string | null>(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+
   return (
-    <button className="text-black border border-black px-4 py-2 rounded-none">
-      X
+    <button
+      className="w-24 h-24 text-black border border-black px-4 py-2 rounded-none"
+      onClick={handleClick}
+    >
+      {value}
     </button>
   );
 }
